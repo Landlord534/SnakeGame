86a4c69c6ab21d48cf1877ddfbb957c4917c45c9
Landlord534 <nikolay.chalykh.job@yandex.ru>
2022-12-29 09:53:16 +0300

    Started to implement animetion for the snake

--- js/snake.js
+++ js/snake.js
@@ -3,6 +3,7 @@ import { SnakePiece } from "./snakePiece.js";
 export class Snake {
     snakePieces = [];
     startPiecesNumber = 3;
+    direction = "Right";
 
     constructor(context, blockSize, startX, startY) {
         this.context = context;
@@ -23,6 +24,29 @@ export class Snake {
             this.context.fillStyle = "green";
             this.context.fill();
             this.context.closePath();
+        }
+    }
+
+    move() {
+        const headIndex = 0;
+        for(let i = this.snakePieces.length - 1; i > 0; i--) {
+            this.snakePieces[i].x = this.snakePieces[i - 1].x;
+            this.snakePieces[i].y = this.snakePieces[i - 1].y;
+        }
+
+        switch(this.direction) {
+            case "Right": 
+                this.snakePieces[headIndex].x += this.blockSize;
+                break;
+            case "Left": 
+                this.snakePieces[headIndex].x -= this.blockSize;
+                break;
+            case "Up":
+                this.snakePieces[headIndex].y -= this.blockSize;
+                break;
+            case "Down":
+                this.snakePieces[headIndex].y += this.blockSize;
+                break;
         }
     }
 }
--- js/snakeGame.js
+++ js/snakeGame.js
@@ -11,10 +11,42 @@ const centerPointY = centerPointX;
 const playground = new Playground(context, canvas.width, canvas.clientHeight, blockSize);
 const snake = new Snake(context, blockSize, centerPointX, centerPointY);
 
+document.addEventListener("keydown", keyDownHandler, false);
+document.addEventListener("keyup", keyUpHandler, false);
+document.addEventListener("keyleft", keyLeftHandler, false);
+document.addEventListener("keyright", keyRightHandler, false);
+
+function keyDownHandler(e) {
+    if (snake.direction != "Up") {
+        snake.direction = "Down";
+    }
+}
+
+function keyUpHandler(e) {
+    if (snake.direction != "Down") {
+        snake.direction = "Up";
+    }
+}
+
+function keyLeftHandler(e) {
+    if (snake.direction != "Right") {
+        snake.direction = "Left";
+    }
+}
+
+function keyRightHandler(e) {
+    if (snake.direction != "Left") {
+        snake.direction = "Right";
+    }
+}
+
 function draw() {
+    context.clearRect(0, 0, canvas.width, canvas.height);
     playground.drawStepGrid();
     snake.draw();
-
+    snake.move();
 }
+
+setInterval(draw, 1000);
 
 draw();
